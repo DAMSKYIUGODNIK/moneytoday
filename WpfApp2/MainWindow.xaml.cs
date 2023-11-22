5d3bddb34b67b966e0067789ca6b45ba9fc312e2
Your Name <you@example.com>
2023-11-22 13:35:43 +0700

    first commit

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Объявление сущностей для заказа еды
+public class FoodItem
+{
+    public int FoodItemId { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public string Category { get; set; }
+}
+
+public class Order
+{
+    public int OrderId { get; set; }
+    public DateTime OrderDate { get; set; }
+    public decimal TotalAmount { get; set; }
+    public List<FoodItem> FoodItems { get; set; }
+}
+
+// Обработчик запроса на заказ еды
+public class FoodOrderHandler
+{
+    // Метод для создания нового заказа
+    public Order CreateOrder()
+    {
+        // Создаем список позиций еды
+        List<FoodItem> foodItems = new List<FoodItem>()
+        {
+            new FoodItem() { FoodItemId = 1, Name = "Пицца", Price = 10.99m, Category = "Итальянская" },
+            new FoodItem() { FoodItemId = 2, Name = "Суши", Price = 15.99m, Category = "Японская" },
+            new FoodItem() { FoodItemId = 3, Name = "Бургер", Price = 8.99m, Category = "Американская" }
+        };
+
+        // Создаем объект заказа
+        Order order = new Order() { OrderId = 1, OrderDate = DateTime.Now, TotalAmount = 35.97m, FoodItems = foodItems };
+
+        return order;
+    }
+}
